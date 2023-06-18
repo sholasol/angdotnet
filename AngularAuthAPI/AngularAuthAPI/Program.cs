0b130ed7b55e843c2298289c1434c682a6a7475b
Solomon <sholasolomon@yahoo.com>
2023-06-18 18:09:27 +0100

    Angular Frontend and WebAPI login and signup

--- AngularAuthAPI/AngularAuthAPI/Program.cs
+++ AngularAuthAPI/AngularAuthAPI/Program.cs
@@ -1,4 +1,11 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using AngularAuthAPI.Context;
+using Microsoft.Extensions.DependencyInjection;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
 
 // Add services to the container.
 
@@ -6,6 +13,31 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//add Cors
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("MyPolicy", builder =>
+    {
+        builder.AllowAnyOrigin();
+        builder.AllowAnyMethod();
+        builder.AllowAnyHeader();
+    });
+});
+
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("MySqlConnStr"),
+        new MySqlServerVersion(new Version(10, 4, 28)),//phpmyadmin version 10.4.28
+        mySqlOptions =>
+        {
+            mySqlOptions.EnableRetryOnFailure();
+        });
+});
+
+
 
 var app = builder.Build();
 
@@ -18,6 +50,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors("MyPolicy"); //register cors in the pipeline
+
+
+app.UseAuthentication(); //add using authentication
 app.UseAuthorization();
 
 app.MapControllers();
